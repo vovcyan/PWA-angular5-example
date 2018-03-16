c1c0f4573ed706515022dc5bbd36c65655fd8c95
Vladimir Fedin <vovcyan@MacBook-Pro-3.local>
2018-03-16 13:04:01 +0400

    Init commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    public title = 'app';
+    public isOnline: boolean = navigator.onLine;
+}
